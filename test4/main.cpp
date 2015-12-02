8e12e3bfd8d5c4fbb709964e5aa41d9b07d804b7
lzh11 <2239040999@qq.com>
2015-12-02 20:46:35 +0800

    test4
    数组越界

--- test4/main.cpp
+++ test4/main.cpp
@@ -10,6 +10,15 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    int a[3]={1,2,3};
+    int b[4];
+    int i;
+    for(i=1;i<100;i++)
+    {
+        b[i]=i;
+    }
+    printf("%d %d %d\n",a[0],a[1],a[2]);
+    printf("%x %x\n",a,b);
     std::cout << "Hello, World!\n";
     return 0;
 }
